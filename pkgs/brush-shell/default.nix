5542aff2ca5a4ff8c84a25867aacc2d3fc79aa59
Izaac Zavaleta <izaac.zavaleta@suse.com>
2026-04-11 08:20:39 -0700

    brush-shell: build from source (main branch, 72593cd)
    Switches from pre-built 0.3.0 binary to building from latest main.
    Includes zsh-hooks support and readline macro fixes needed for atuin.

--- pkgs/brush-shell/default.nix
+++ pkgs/brush-shell/default.nix
@@ -2,31 +2,33 @@
   pkgs,
   lib,
   ...
-}:
-pkgs.stdenv.mkDerivation rec {
-  pname = "brush-shell";
-  version = "0.3.0";
+}: let
+  rev = "72593cd56c919d78b49f6ef11b9ed0e26b72b03e";
+  shortRev = builtins.substring 0 7 rev;
+in
+  pkgs.rustPlatform.buildRustPackage {
+    pname = "brush-shell";
+    version = "0.3.0-unstable-${shortRev}";
 
-  src = pkgs.fetchurl {
-    url = "https://github.com/reubeno/brush/releases/download/brush-shell-v${version}/brush-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "sha256-iSWSSSnMq/nrp6gDs7obOnNSvyVR+8me0r1pcNMwd20=";
-  };
+    src = pkgs.fetchFromGitHub {
+      owner = "reubeno";
+      repo = "brush";
+      inherit rev;
+      hash = "sha256-o6QKce4PNeie/D3RDommsWaw4vrei6vyDqs4FAE1DVA=";
+    };
 
-  sourceRoot = ".";
+    cargoHash = "sha256-UCniXYFJP3OmXKFO6hvwVCS1PSXfKwFPWpJvq54+sEM=";
 
-  nativeBuildInputs = [pkgs.autoPatchelfHook];
-  buildInputs = [pkgs.gcc-unwrapped.lib];
+    cargoBuildFlags = ["-p" "brush-shell"];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    install -m 755 brush $out/bin/brush
-  '';
+    # Tests require bash as oracle and network access
+    doCheck = false;
 
-  meta = with lib; {
-    description = "Bash/POSIX-compatible shell written in Rust";
-    homepage = "https://github.com/reubeno/brush";
-    license = licenses.mit;
-    mainProgram = "brush";
-    platforms = ["x86_64-linux"];
-  };
-}
+    meta = with lib; {
+      description = "Bash/POSIX-compatible shell written in Rust";
+      homepage = "https://github.com/reubeno/brush";
+      license = licenses.mit;
+      mainProgram = "brush";
+      platforms = ["x86_64-linux"];
+    };
+  }
